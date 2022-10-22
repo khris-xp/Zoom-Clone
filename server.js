3771134b4786c6cf1a9a2bc050cf76d100f6f6ff
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-10-23 06:22:58 +0700

    add : ejs files

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.set('view engine','ejs');
+
 app.get('/' , (req,res) => {
-    res.send("Hello World!");
+    res.render('room');
 })
 
 app.listen(PORT);
