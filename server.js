a4bf0c7b7fa3ffb80ccca6f58549c264262ddfca
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-10-23 06:42:31 +0700

    add : room id

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-
+const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 5000;
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.get('/' , (req,res) => {
-    res.render('room');
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params })
 })
 
 app.listen(PORT);
